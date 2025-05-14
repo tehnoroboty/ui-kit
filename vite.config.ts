a16f3811ae99b4fd16e4629e7a1ddcac11fe51e3
tehnoroboty <tehnoroboty@gmail.com>
2025-05-14 15:14:53 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'node:path'
+
+import { defineConfig } from 'vite'
+
+import { dependencies, devDependencies } from './package.json'
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
+      formats: ['es'],
+      name: 'inst_ui-kit',
+    },
+    rollupOptions: {
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+      ],
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
+})
