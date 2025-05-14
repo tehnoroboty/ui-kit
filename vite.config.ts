113f216404ff3d828bf7a8d39d5f645ade374a3a
Anton-Osip <osipchika@gmail.com>
2025-05-14 19:31:12 +0300

    fix: fixed error

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,25 @@
-import { resolve } from 'node:path'
+import { resolve } from 'path'
 
 import { defineConfig } from 'vite'
 
 import { dependencies, devDependencies } from './package.json'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      // the proper extensions will be added
-      fileName: 'index',
+      fileName: 'ui_kit',
       formats: ['es'],
-      name: 'inst_ui-kit',
+      name: 'ui_kit',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      // external: ['react', 'react-dom'],
       external: [
         'react/jsx-runtime',
         ...Object.keys(dependencies),
         ...Object.keys(devDependencies),
       ],
-      // output: {
-      //   dir: 'dist',
-      //   entryFileNames: '[name].js',
-      //   format: 'es',
-      // },
     },
     sourcemap: true,
     target: 'esnext',
